b4c032a879bb69b3034148ed5da158ac08208cae
KaiTheRedNinja <88234730+KaiTheRedNinja@users.noreply.github.com>
2023-08-31 20:50:14 +0800

    Fix empty tab issue

--- Sources/GSheetsSwiftTypes/Sheet.swift
+++ Sources/GSheetsSwiftTypes/Sheet.swift
@@ -10,4 +10,20 @@ import Foundation
 public struct Sheet: Codable {
     public var properties: SheetProperties
     public var data: [GridData]
+
+    enum Keys: CodingKey {
+        case properties, data
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: Keys.self)
+        try container.encode(properties, forKey: .properties)
+        try container.encode(data, forKey: .data)
+    }
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: Keys.self)
+        properties = try container.decode(SheetProperties.self, forKey: .properties)
+        data = (try? container.decode([GridData].self, forKey: .data)) ?? []
+    }
 }
