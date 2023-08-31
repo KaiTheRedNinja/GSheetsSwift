2c9d26aee811f54c4a6194d08c11704cf6eee61a
KaiTheRedNinja <88234730+KaiTheRedNinja@users.noreply.github.com>
2023-08-31 20:50:14 +0800

    Fix empty tab issue

--- Sources/GSheetsSwiftTypes/GridData.swift
+++ Sources/GSheetsSwiftTypes/GridData.swift
@@ -13,4 +13,22 @@ public struct GridData: Codable {
     public var rowData: [RowData]
 //    public var rowMetadata: [DimensionProperties]
 //    public var columnMetadata: [DimensionProperties]
+
+    enum Keys: CodingKey {
+        case startRow, startColumn, rowData
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: Keys.self)
+        try container.encode(startRow, forKey: .startRow)
+        try container.encode(startColumn, forKey: .startColumn)
+        try container.encode(rowData, forKey: .rowData)
+    }
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: Keys.self)
+        startRow = try container.decode(Int.self, forKey: .startRow)
+        startColumn = try container.decode(Int.self, forKey: .startColumn)
+        rowData = (try? container.decode([RowData].self, forKey: .rowData)) ?? []
+    }
 }
